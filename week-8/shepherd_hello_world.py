eb02f0980513a90d965822647201854a406b52d0
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-06-15 10:00:34 -0500

    added hello-world.py

---
+++ week-8/shepherd_hello_world.py
@@ -0,0 +1,9 @@
+#==============================================================================
+# Title:  shepherd_hello_world.py
+# Author: Jeff Shepherd
+# Date:   6/15/2020
+# Modified By:
+# Description: hello world
+#==============================================================================
+
+print("Hello world")
