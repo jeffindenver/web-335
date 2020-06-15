c77d4596a7fe8383f8e5b1054a58300c1395aa8f
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-06-15 10:34:21 -0500

    added shepherd_calculator.py

---
+++ week-8/shepherd_calculator.py
@@ -0,0 +1,21 @@
+#==============================================================================
+# Title:  shepherd_calculator.py
+# Author: Jeff Shepherd
+# Date:   6/15/2020
+# Modified By:
+# Description: calculator
+#==============================================================================
+
+def add(a, b):
+  return a + b
+
+def subtract(a, b):
+  return a - b
+
+def divide(a, b):
+  return a / b
+
+print(add(1, 2))
+print(subtract(4, 1))
+print(divide(8, 2))
+
